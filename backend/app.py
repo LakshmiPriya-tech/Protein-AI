fc64a6f2d927eb129155879fc08f642903be7c8b
Lakshmi Priya <lakshmi.priya.mail@gmail.com>
2026-05-18 19:35:54 +0530

    added cors support

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app = FastAPI(title="ProteinAI Decision API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "https://protein-ai-drab.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
